1dc93f729a0cf1e6e0bc49350c15027159f4c423
alessandro <moretti@gianmarialessandro.com>
2022-06-16 23:15:58 +0200

    counting duplicates

--- tests/countingDuplicates.js
+++ tests/countingDuplicates.js
@@ -1,35 +1,32 @@
-// Count the number of Duplicates
-// Write a function that will return the count of distinct case-insensitive 
-// alphabetic characters and numeric digits that occur more than once in the input string. 
-// The input string can be assumed to contain only alphabets (both uppercase and lowercase)
-// and numeric digits.
+{// Count the number of Duplicates
+  // Write a function that will return the count of distinct case-insensitive 
+  // alphabetic characters and numeric digits that occur more than once in the input string. 
+  // The input string can be assumed to contain only alphabets (both uppercase and lowercase)
+  // and numeric digits.
 
-// Example
-// "abcde" -> 0 # no characters repeats more than once
-// "aabbcde" -> 2 # 'a' and 'b'
-// "aabBcde" -> 2 # 'a' occurs twice and 'b' twice (`b` and `B`)
-// "indivisibility" -> 1 # 'i' occurs six times
-// "Indivisibilities" -> 2 # 'i' occurs seven times and 's' occurs twice
-// "aA11" -> 2 # 'a' and '1'
-// "ABBA" -> 2 # 'A' and 'B' each occur twice
+  // Example
+  // "abcde" -> 0 # no characters repeats more than once
+  // "aabbcde" -> 2 # 'a' and 'b'
+  // "aabBcde" -> 2 # 'a' occurs twice and 'b' twice (`b` and `B`)
+  // "indivisibility" -> 1 # 'i' occurs six times
+  // "Indivisibilities" -> 2 # 'i' occurs seven times and 's' occurs twice
+  // "aA11" -> 2 # 'a' and '1'
+  // "ABBA" -> 2 # 'A' and 'B' each occur twice
+}
+var txt = "aabBcde112311cC2"
 
-var txt = "aabBcde112311"
-
-function duplicateCount(text){
-    //...
-    textArr = text.toLowerCase().split("")
-    const duplicate = {}
-    let idx = 0
-    let i = 0
-    textArr.forEach(e => {
-      duplicate[e] = duplicate[e] + 1 || 1
-      if(duplicate)
-      console.log(duplicate[e])
-
-    });
-    
-    
-    
-  }
+function duplicateCount(text) {
+  //...
+  textArr = text.toLowerCase().split("")
+  const duplicate = {}
+  let i = 0
+  textArr.forEach(e => {
+    duplicate[e] = duplicate[e] + 1 || 1
+    if (duplicate[e] > 1 && duplicate[e] < 3) {
+      i += 1;
+    }
+  });
+  return i;
+}
 
 duplicateCount(txt)
