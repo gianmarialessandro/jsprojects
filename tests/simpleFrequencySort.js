e749d628deb41c67cee618bd590eb2a96817991e
alessandro <moretti@gianmarialessandro.com>
2022-06-16 14:10:35 +0200

    frequency sort

---
+++ tests/simpleFrequencySort.js
@@ -0,0 +1,23 @@
+var nrs = [2,3,5,3,7,9,5,3,7]
+
+
+function solve(arr){
+    //..
+    let i = 0;
+    let sortedArr = [];
+    let arrayKey = [];
+    for (let index = 0; index < arr.length; index++) {
+        const e = array[index];
+        for (let indexN = 0; indexN < sortedArr.length; indexN++) {
+            const nE = sortedArr[indexN];
+            if(e === ne){
+
+            }
+            
+        }
+
+        
+    }
+  }
+
+solve(nrs)
