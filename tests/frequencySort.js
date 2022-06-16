7bbf006e4b04af0a3cc9c2a88327be4af1617d57
gianmarialessandro <gianmarialessandro@gmail.com>
2022-06-16 21:51:10 +0200

    frequency sort

--- tests/frequencySort.js
+++ tests/frequencySort.js
@@ -1,48 +1,34 @@
 var nrs = [2, 3, 5, 3, 7, 9, 5, 3, 7];
 
 function solve(arr) {
-  var nrsSorted = arr.sort((a, b) => a - b)
-  var arrFrq = []
-  nrsSorted.map((e) => {
-    
-  })
-  console.log(nrsSorted)
-
-  // let nrsSorted = nrs.sort((a, b) => a - b)
-  // var obj = {}
-  // var arrObj = []
-  // for (let i = 0; i < nrsSorted.length; i++) {
-  //   const e = nrsSorted[i];
-  //   if (obj[e]){
-  //     obj[e].frq += 1;
-  //   } else {
-  //     obj[e] = { nr: e, frq: 1}
-  //     arrObj.push(obj[e])
-  //   }    
-  // }
-  // let p = arrObj.sort((a, b) => a.frq - b.frq).reverse()
-  // let f = p.sort((a, b) => {
-  //   if(a.frq === b.frq) {
-  //     a - b
-  //   }
-  //   console.log(a, b)
-  // })
-
-  // console.log(f)
-  
-  // var arrObj = [];
-  // var objNr = {};
-  // for (let i = 0; i < arr.length; i++) {
-  //   const e = arr[i];
-  //   if(objNr[e]) {
-  //     objNr[e].frq += 1
-  //   } else {
-  //     objNr[e] = { nr: 0, frq: 1 }
-  //     objNr[e].nr = e 
-  //     arrObj.push(objNr[e])
-  //   }
-  // }
-  // console.log(arrObj)
+  const frq = {}; 
+  arr.forEach(e => {
+    frq[e] = frq[e] + 1 || 1; 
+  });
+  return [...arr].sort((a, b) => frq[b] - frq[a] || a -b) 
 }
 
 solve(nrs);
+
+// OTHER SOLUTIONS 
+
+//
+function solve1(arr){
+  var r={}
+  for(var n of arr) r[n]=r[n]+1||1
+  return arr.slice().sort((a,b)=>r[b]-r[a]||a-b)
+}
+
+
+//
+function solve2(arr) {
+  let dict = new Map()
+  for (let x of arr) {
+    dict.set(x, (dict.get(x) || 0) + 1)
+  }
+  return arr.sort((x, y) => {
+    let n = dict.get(x)
+    let m = dict.get(y)
+    return n === m ? x - y : m - n
+  })
+}
