bab0ce4a8a6b814cd79813c8b23118345e8917f3
alessandro <moretti@gianmarialessandro.com>
2022-06-22 14:40:34 +0200

    bellTolls

--- tests/bellTolls.js
+++ tests/bellTolls.js
@@ -1,34 +1,74 @@
-const test = 13;
+const test = 3;
+
+// const bell = (nr) => {
+//     let n = nr
+//     var res = [];
+//     for (let x = 1; x <= n; x++) {
+//         let nrs = [];
+//         let z = x
+//         let p = x 
+//         for (let y = x; y < x + x; y++) {
+//             if(y === x) 
+//             {
+//                 // console.log(y)
+//                 nrs.push(y)        
+
+//             } else 
+//             {
+//                 z -= 2;
+//                 p += z
+//                 nrs.push(p)
+//             }
+
+//         }
+//         res.push(nrs)
+
+//     }
+//     // res.shift()
+//     console.log(res)
+
+//     return res;
+// }
 
 const bell = (nr) => {
-    let n = nr
+    let x = nr
     var res = [];
-    for (let x = 0; x < n; x++) {
-        let nrs = [];
-        let z = x
-        let p = x 
-        for (let y = x; y < x + x; y++) {
-            if(y === x) 
-            {
-                // console.log(y)
-                nrs.push(y)        
+    let nrs = [];
+    let z = x
+    let p = x
 
-            } else 
-            {
-                z -= 2;
-                p += z
-                nrs.push(p)
-            }
-            
+    for (let y = x; y < x + x; y++) {
+        if (y === x) {
+            // console.log(y)
+            nrs.push(y)
+
+        } else {
+            z -= 2;
+            p += z
+            nrs.push(p)
         }
-        res.push(nrs)
-        
+
     }
-    res.shift()
-    // console.log(res)
+    res.push(nrs)
+
+
+    // res.shift()
+    console.log(res)
 
     return res;
 }
 
 
 bell(test);
+
+// OTHER SOLUTIONS
+
+//
+const bell1=n=>[...Array(n)].map((_,i)=>++i*n--)
+
+//
+function bell2(n) {
+    let add = n, curr = 0;
+    const half = [...Array(Math.ceil(n / 2))].map((_, i) => n + (!i ? 0 : curr += add -= 2));
+    return half.concat([...half].reverse().slice(n % 2));
+  }
