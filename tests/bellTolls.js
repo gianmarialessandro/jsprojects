08a9df9cce0d8b6aa4dcfc596ab32ac8db86a7bb
alessandro <moretti@gianmarialessandro.com>
2022-06-22 13:11:04 +0200

    bellTolls

--- tests/bellTolls.js
+++ tests/bellTolls.js
@@ -1,41 +1,33 @@
 const test = 13;
 
-// const bell = (n) => {
-//     let nrToAdd = n - 2
-//     var index = 0;
-//     var res = [];
-//     if (n % 2 === 0) {
-//         index =  n / 2  
-//         for (let i = 0; i < index; i++) {
-//             res.push((n + i))            
-//         }
-//     } else 
-//         index = Math.floor((n / 2) + 1);
-            
-    
-
-//     console.log(nrToAdd, index, res)
-// }
 const bell = (nr) => {
     let n = nr
     var res = [];
-    if (n !== 0) {
-        for (let i = 1; i <= n; i++) {
-            let nrs = [];
-            let p = i * 2
-            for (let ii = i; ii <= p; ii++) {
+    for (let x = 0; x < n; x++) {
+        let nrs = [];
+        let z = x
+        let p = x 
+        for (let y = x; y < x + x; y++) {
+            if(y === x) 
+            {
+                // console.log(y)
+                nrs.push(y)        
 
-                nrs.push(ii)
-                
+            } else 
+            {
+                z -= 2;
+                p += z
+                nrs.push(p)
             }
-            res.push(nrs)
-
+            
         }
-
-        // res.shift();
-        console.log(res)
+        res.push(nrs)
+        
     }
+    res.shift()
+    // console.log(res)
 
+    return res;
 }
 
 
