00bc283c153735d3b93f0bde20deb3817fad72fd
alessandro <moretti@gianmarialessandro.com>
2022-07-07 21:29:21 +0200

    mexican wave

--- codeWars_Kata/mexicanWave.js
+++ codeWars_Kata/mexicanWave.js
@@ -16,14 +16,57 @@ const grido = "Hello"
 
 function wave(str){
     // Code here
-    let arr = []
-    let a = str.toLowerCase();
-    for(let i = 0; i < a.length; i++) {
-      let onda = a[i].toUpperCase()
-      arr.push(a[i])
-    }
+    var nArr = []
+    var nStr = str.toLowerCase()
     
-    console.log(arr)
-  }
+    for (let i = 0; i < nStr.length; i++) {
+      let lStr = ""
+      if (nStr[i] == " ") continue 
+      for (let o = 0; o < nStr.length; o++) {
+        
+        if (o == i) {
+          let up = nStr[o].toUpperCase()
+          lStr += up
+        } else {
+          lStr += nStr[o]
+        }
+      }
+      nArr.push(lStr)
+    }
+    console.log(nArr)
+    return nArr
+
+}
 
 wave(grido)
+
+// OTHER SOLUTION 
+//
+function wave(str){
+  let result = [];
+  
+  str.split("").forEach((char, index) => {
+      if (/[a-z]/.test(char)) {
+          result.push(str.slice(0, index) + char.toUpperCase() + str.slice(index + 1));
+      }
+  });
+  
+  return result;
+}
+
+// 
+function wave(str){
+  let newArr = [];
+  for (let i = 0; i < str.length; i++) {
+    let copy = str.split('');
+    if(copy[i] !== ' ') {
+    copy[i] = copy[i].toUpperCase()
+    newArr.push(copy.join(''))
+    }
+  }
+  return newArr
+}
+
+//
+var wave=w=>[...w].map((a,i)=>w.slice(0,i)+a.toUpperCase()+w.slice(i+1)).filter(a=>a!=w)
+
