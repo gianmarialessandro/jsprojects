295196345d739831391d107ae7c237b51a41bab9
almoretti <alessandro.moretti@avm.de>
2022-08-17 17:04:11 +0200

    commit: test numero 4
    Con questo si vuole essere sicuri delle righe di separazione fra titolo e testo del commit
    
    task la solita di prima

--- testForcommit/commitprova.js
+++ testForcommit/commitprova.js
@@ -1,3 +1,5 @@
-const commit = "primo commit non ha letto la prima riga di separazione"
-const commit2 = "neanche il secondo"
-var commit3 = "adesso si prova a separare i due messaggi"
+const commit = "primo commit non ha letto la prima riga di separazione";
+const commit2 = "neanche il secondo";
+var commit3 = "adesso si prova a separare i due messaggi";
+let commitEsimo =
+  "proviamo a a confermare il doppio passo -m virgolette piu -m virgolette";
